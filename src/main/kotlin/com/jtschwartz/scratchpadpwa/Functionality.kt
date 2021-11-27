962b1b392beb5bb4a1cf90cb9b1031b3fa0a21a4
Jacob Schwartz <jacob@jtschwartz.com>
2021-11-27 13:05:39 -0500

    Functionality

---
+++ src/main/kotlin/com/jtschwartz/scratchpadpwa/Functionality.kt
@@ -0,0 +1,19 @@
+package com.jtschwartz.scratchpadpwa
+
+import com.vaadin.flow.component.UI
+
+import com.vaadin.flow.dom.ThemeList
+import com.vaadin.flow.theme.material.Material
+
+
+object Functionality {
+	fun toggleTheme() {
+		val themeList: ThemeList = UI.getCurrent().element.themeList
+		
+		if (themeList.contains(Material.DARK)) {
+			themeList.remove(Material.DARK)
+		} else {
+			themeList.add(Material.DARK)
+		}
+	}
+}
